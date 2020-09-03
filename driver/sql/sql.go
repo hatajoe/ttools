379324183babae066251cc88e413e32484366c12
Yusuke Hatanaka <arbalestimp@gmail.com>
2020-09-03 22:11:43 +0900

    reset auto increment value and make it reset DB just once

--- driver/sql/sql.go
+++ driver/sql/sql.go
@@ -8,25 +8,35 @@ import (
 	"log"
 	"regexp"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	proxy "github.com/shogo82148/go-sql-proxy"
 )
 
 var (
-	driverName        string = "tsql"
+	db                *sql.DB
 	sqlTracingEnabled bool
-	regexpNewLine     *regexp.Regexp                     = regexp.MustCompile(`\n`)
-	regexpInsert      *regexp.Regexp                     = regexp.MustCompile(`INSERT`)
-	regexpTable       *regexp.Regexp                     = regexp.MustCompile(`INSERT INTO(.*)\(.*VALUES`)
-	inserted          map[*proxy.Conn]map[string][]int64 = map[*proxy.Conn]map[string][]int64{}
-	deletionQuery     string                             = `DELETE FROM %s WHERE id = ?`
+	openCount         int64
+	driverName        string             = "tsql"
+	mu                *sync.Mutex        = &sync.Mutex{}
+	regexpNewLine     *regexp.Regexp     = regexp.MustCompile(`\n`)
+	regexpInsert      *regexp.Regexp     = regexp.MustCompile(`INSERT`)
+	regexpTable       *regexp.Regexp     = regexp.MustCompile(`INSERT INTO(.*)\(.*VALUES`)
+	inserted          map[string][]int64 = map[string][]int64{}
+	deletionQuery     string             = `DELETE FROM %s WHERE id = ?`
 )
 
 // Open returns *sql.DB which contained following hooks
 // SQL Tracing: show executed queries and processing time by query. no display is default.
 // Auto Deletion: delete all inserted records after close the connection.
 func Open(driver d.Driver, dataSource string) (*sql.DB, error) {
+	atomic.AddInt64(&openCount, 1)
+	if db != nil {
+		return db, nil
+	}
+
 	alreadyRegistered := false
 	drivers := sql.Drivers()
 	for _, dn := range drivers {
@@ -54,32 +64,38 @@ func SetHooks(driver d.Driver) d.Driver {
 			q := regexpNewLine.ReplaceAllString(stmt.QueryString, "")
 			uq := strings.ToUpper(q)
 			if regexpInsert.MatchString(uq) {
+				mu.Lock()
+				defer mu.Unlock()
+
 				cap := regexpTable.FindStringSubmatch(uq)
 				if len(cap) < 2 {
 					return fmt.Errorf("failed to parse table name from query: %s", stmt.QueryString)
 				}
 				table := strings.ToLower(strings.TrimSpace(cap[1]))
-				if _, ok := inserted[stmt.Conn]; !ok {
-					inserted[stmt.Conn] = map[string][]int64{}
-				}
-				if _, ok := inserted[stmt.Conn][table]; !ok {
-					inserted[stmt.Conn][table] = []int64{}
+				if _, ok := inserted[table]; !ok {
+					inserted[table] = []int64{}
 				}
 				if res != nil {
 					lastInsertId, err := res.LastInsertId()
 					if err != nil {
 						return err
 					}
-					inserted[stmt.Conn][table] = append(inserted[stmt.Conn][table], lastInsertId)
+					inserted[table] = append(inserted[table], lastInsertId)
 				}
 			}
 			return nil
 		},
 		PreClose: func(c context.Context, conn *proxy.Conn) (interface{}, error) {
-			if _, ok := inserted[conn]; !ok {
+			mu.Lock()
+			defer mu.Unlock()
+
+			if 0 < atomic.AddInt64(&openCount, -1) {
 				return nil, nil
 			}
-			for table, ids := range inserted[conn] {
+			if _, err := conn.ExecContext(c, `SET foreign_key_checks = 0`, nil); err != nil {
+				return nil, err
+			}
+			for table, ids := range inserted {
 				if len(ids) <= 0 {
 					continue
 				}
@@ -94,6 +110,9 @@ func SetHooks(driver d.Driver) d.Driver {
 							return nil, err
 						}
 					}
+				}
+				if _, err := conn.ExecContext(c, fmt.Sprintf("ALTER TABLE %s auto_increment = 1", table), nil); err != nil {
+					return nil, err
 				}
 			}
 			return nil, nil
